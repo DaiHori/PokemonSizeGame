969bfb45eb858fdb47673d4f455c1a08fd0f20f9
daiki-nishimura <72265603+daiki-nishimura@users.noreply.github.com>
2021-06-30 18:20:46 +0900

    footerの追加 (#8)

---
+++ web/src/components/Footer.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-footer dark padless>
+    <v-card color="white" class="flex" flat tile>
+      <v-card-title class="black">
+        <v-layout align-center justify-center>
+          <v-btn v-for="item in items" :key="item" icon @click="moveLink">
+            <v-icon class="position" size="68px">{{ item.icon }}</v-icon>
+          </v-btn>
+        </v-layout>
+      </v-card-title>
+
+      <v-card-text class="py-2 black--text text-center">
+        ©{{ new Date().getFullYear() }} — <strong>DaiHori</strong>
+      </v-card-text>
+    </v-card>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  methods: {
+    moveLink: () => {
+      window.open("https://github.com/DaiHori/PokemonSizeGame", "_blank");
+    },
+  },
+  data: () => ({
+    items: [
+      {
+        icon: "mdi-github",
+      },
+    ],
+  }),
+};
+</script>
+
+<style lang="scss">
+.position {
+  float: right;
+}
+</style>
--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,15 +1,20 @@
 <template>
-  <HeaderViews />
+  <div>
+    <HeaderViews />
+    <Footer />
+  </div>
 </template>
 
 <script>
 import HeaderViews from "../components/HeaderViews";
+import Footer from "../components/Footer";
 
 export default {
   name: "Home",
 
   components: {
     HeaderViews,
+    Footer,
   },
 };
 </script>
