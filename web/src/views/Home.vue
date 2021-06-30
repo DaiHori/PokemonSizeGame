abf5e973b8cefb7a29d2b8f0e00a6c4db0134741
daiki-nishimura <daikiahe0@gmail.com>
2021-06-30 16:26:55 +0900

    refactor: ContentsViewsの追加

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,15 +1,20 @@
 <template>
-  <HeaderViews />
+  <div>
+    <HeaderViews />
+    <ContentsViews />
+  </div>
 </template>
 
 <script>
 import HeaderViews from "../components/HeaderViews";
+import ContentsViews from "../components/ContentsViews";
 
 export default {
   name: "Home",
 
   components: {
     HeaderViews,
+    ContentsViews,
   },
 };
 </script>
