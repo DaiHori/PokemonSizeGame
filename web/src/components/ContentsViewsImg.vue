872c2893f5fe826b41807768c515a2fc06e598d3
daiki-nishimura <daikiahe0@gmail.com>
2021-07-07 21:25:02 +0900

    refactor: img components作成

---
+++ web/src/components/ContentsViewsImg.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <v-layout>
+      <div class="size align-end" v-for="img in imgs" :key="img.imgs">
+        <v-img :src="img.img1" />
+      </div>
+    </v-layout>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ContentsViewsImg",
+
+  data: () => ({
+    imgs: [
+      {
+        img1: require("@/assets/ピカチュウ_なみのり.gif"),
+        img2: require("@/assets/イーブイ.gif"),
+      },
+    ],
+  }),
+};
+</script>
+
+<style lang="scss">
+.size {
+  width: 10%; /* 横幅を割合で指定 */
+  height: auto; /* 高さは自動指定 */
+}
+.position {
+  float: right;
+}
+</style>
