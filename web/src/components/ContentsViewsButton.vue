bd30c65673da3039bceb17f776c706c9e2435c7f
daiki-nishimura <daikiahe0@gmail.com>
2021-07-07 14:31:05 +0900

    refactor: ContentsViewsBtnコンポーネントを作成

---
+++ web/src/components/ContentsViewsButton.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col class="text">
+        <v-btn v-for="pokemon in 1" :key="pokemon.name"
+          ><h1>{{ ramdom() }}</h1></v-btn
+        >
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import POKEMON from "@/common/pokemon";
+
+export default {
+  name: "ContentsViewsButton",
+
+  methods: {
+    ramdom: function() {
+      const ary = Math.floor(Math.random() * this.pokemons.length);
+      return this.pokemons[ary].name;
+    },
+    // answer: function() {
+    //     const
+    // }
+  },
+  data: () => ({
+    pokemons: POKEMON.POKEMONS,
+  }),
+};
+</script>
