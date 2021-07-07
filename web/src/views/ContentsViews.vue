915d6f38a1f6c3f1e6db11c4acb033d3ba66feb5
daiki-nishimura <daikiahe0@gmail.com>
2021-07-07 21:26:32 +0900

    refactor: 画像

--- web/src/views/ContentsViews.vue
+++ web/src/views/ContentsViews.vue
@@ -1,8 +1,6 @@
 <template>
   <v-card class="mx-auto mt-2" max-width="1280" height="500">
-    <v-layout class="size align-end">
-      <v-img src="@/assets/ピカチュウ_なみのり.gif" />
-    </v-layout>
+    <Img />
 
     <v-card-text class="text-center">
       <h1>第{{ $store.state.counter }}問</h1>
@@ -15,33 +13,24 @@
       <Button />
       <br />
       <Button />
+      <Img2 />
     </v-card-text>
     <v-card-actions> </v-card-actions>
-
-    <v-layout class="size position">
-      <v-img src="@/assets/イーブイ.gif" />
-    </v-layout>
   </v-card>
 </template>
 
 <script>
 import Button from "@/components/ContentsViewsButton.vue";
+import Img from "@/components/ContentsViewsImg.vue";
+import Img2 from "@/components/ContentsViewsImg2.vue";
 
 export default {
   name: "ContentsViews",
 
   components: {
     Button,
+    Img,
+    Img2,
   },
 };
 </script>
-
-<style lang="scss">
-.size {
-  width: 10%; /* 横幅を割合で指定 */
-  height: auto; /* 高さは自動指定 */
-}
-.position {
-  float: right;
-}
-</style>
