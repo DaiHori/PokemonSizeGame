1579df3d88685713ad4e0a24459f96219ba4d7bf
daiki-nishimura <daikiahe0@gmail.com>
2021-07-07 21:26:11 +0900

    feat: 正解と不正解画面の追加

---
+++ web/src/views/IncorrectAnswer.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <HeaderViews />
+    <v-card class="mx-auto mt-2" max-width="1280" height="500">
+      <v-card-text class="text-center">
+        <h1>不正解</h1>
+      </v-card-text>
+      <v-card-actions> </v-card-actions>
+    </v-card>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import HeaderViews from "./HeaderViews";
+import Footer from "./Footer";
+
+export default {
+  name: "IncorrectAnswer",
+
+  components: {
+    HeaderViews,
+    Footer,
+  },
+};
+</script>
